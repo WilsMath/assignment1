411cd42e19b24f1d816c63bebc2ecd81624fff1d
Mathias <wilson.mathias@capgemini.com>
2024-10-22 16:01:16 +0530

    Initial commit

---
+++ src/main/java/com/example/WebApp1/Controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.WebApp1.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
